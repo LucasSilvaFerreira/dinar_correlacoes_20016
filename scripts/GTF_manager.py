eb4e04dd9b1b3df14afabfd5cb04264ffd35c83f
lucas <bioinfolucas@gmail.com>
2016-01-13 18:56:06 -0200

    retirar coisas do dinar do main

--- scripts/GTF_manager.py
+++ scripts/GTF_manager.py
@@ -3,24 +3,23 @@ __author__ = 'lucas'
 import sys
 import re
 from tqdm import tqdm
-from collections import namedtuple
-
+from pybedtools import BedTool, tempfiles
+import copy
 
 
 class Transcript():
     def __init__(self, transcript_name, exons_array, attr_list):
         self.transcript_name = transcript_name
         self.exons = exons_array
+        self.gene_info = self.exons[0][1]
+        self.strand =  self.exons[0][6]
         self.__attr_list = attr_list
         self.exon_count = len(exons_array)
-        self.transcript_size = self.__get_transcrip_size()
-        #print self.transcript_name, self.transcript_size
+        self.transcript_length = self.__get_transcrip_size()
         self.attrs = self.__parse_attrs() # aqui eu deixarei todos os atributos dos genes em um dict
-
 
     def exon_size(self, one_exon_in_array):
         return int(one_exon_in_array[4])-int(one_exon_in_array[3])
-
 
     def __get_transcrip_size(self):
         return sum([self.exon_size(one_exon_in_array=exon) for exon in self.exons])
@@ -39,6 +38,25 @@ class Transcript():
 
         return hash_to_load_return
 
+    def locus_coords(self):
+        chr = self.exons[0][0]
+        strand = self.exons[0][6]
+
+
+        if strand == "-":
+
+            return (chr,
+            min([int(end[3]) for end in self.exons]),
+            max([int(start[4]) for start in self.exons]))
+        elif strand == "+":
+            return (chr,
+            min([int(start[3]) for start in self.exons]),
+            max([int(end[4])  for end in self.exons]))
+
+
+    def get_bed6(self):
+        chr, start, end = map(str, self.locus_coords())
+        return '\t'.join([chr, start, end, self.transcript_name, self.gene_info, self.strand])
 
 class Gene_content():
     def __init__(self, gene_id,
@@ -55,6 +73,11 @@ class Gene_content():
         self.__child_transcript_possible_fields = attr_transcript_field #hash com os valores que os transcritos vao possuir
         self.__transcripts_list = self.__parse_transcripts()
         self.number_of_transcripts = len(self.__transcripts_list)
+
+    def get_bed6(self):
+        chr, start, end = map(str, self.locus_coords())
+        return '\t'.join([chr, start, end, self.gene_id, self.gene_info, self.strand])
+
 
     def locus_coords(self):
         #print type(self.__transcripts_list), self.__transcripts_list
@@ -85,14 +108,11 @@ class Gene_content():
             out_ids_trans.append(re.search('transcript_id "(\S*)"', trans_line[-1]).group(1))
         return out_ids_trans
 
-
-
     def __parse_transcripts(self):
         hash_transcripts_to_parse = {}
         return [Transcript(transcript_name=ts_key,
                            exons_array=ts_values_array,
                            attr_list=self.__child_transcript_possible_fields) for ts_key, ts_values_array in self.transcripts_ids.iteritems()]
-
 
 class GTF_manager():
     def __init__(self, gtf_file):
@@ -122,6 +142,7 @@ class GTF_manager():
                     raise IOError("\n The Line: \n{line}\n don't have a regular gene_id field!".format(line=line))
 
         gene_list_to_return = {}
+        print '\n'
         print 'Converting gene to proper format...'
         for gene_unpack in tqdm(hash_gtf_file):
             key_gene = gene_unpack
@@ -160,6 +181,33 @@ class GTF_manager():
     def gene_list(self):
         return [gene_content  for gene_content in self.genes_hash.itervalues()]
 
+    def gene_list_to_bed6(self, file_name=None, save_in_file=False ):
+        bed6_genes = [gene_to_b6.get_bed6() for gene_to_b6 in  self.gene_list()]
+        # aqui deve entrar algum distema de filtros
+        bed6_genes = BedTool('\n'.join(bed6_genes), from_string=True).sort()
+
+        if not save_in_file:
+            return bed6_genes
+        else:
+            if file_name:
+                return bed6_genes.saveas(fn=file_name, trackline="track name='Genes {}' color=128,0,0".format(file_name.split('/')[-1]))
+            else:
+                raise IOError('\nthe file_name method function needs a name or complete file path with name.\n')
+
+    def gene_list_select(self, arr_gene_id_to_select):
+        ids_not_fount = []
+        hash_to_modify = {}
+        for remove_id in arr_gene_id_to_select:
+            try:
+                hash_to_modify[remove_id] = self.genes_hash[remove_id]
+            except:
+                ids_not_fount.append(remove_id)
+                print remove_id
+        if len(ids_not_fount) >0:
+            print len(ids_not_fount), 'Ids não encontrados...'
+        self.genes_hash = hash_to_modify
+        return self
+
     def transcripts_list(self):
         transcript_list_return = []
         for gene_in_list in self.gene_list():
@@ -167,27 +215,113 @@ class GTF_manager():
                 transcript_list_return.append(transcript_in_list)
         return transcript_list_return
 
+    def transcripts_list_to_bed6(self, file_name=None, save_in_file=False ):
+        bed6_trans = [trans_to_b6.get_bed6() for trans_to_b6 in  self.transcripts_list()]
+        # aqui deve entrar algum distema de filtros
+        bed6_trans = BedTool('\n'.join(bed6_trans), from_string=True).sort()
+
+        if not save_in_file:
+            return bed6_trans
+        else:
+            if file_name:
+                return bed6_trans.saveas(fn=file_name, trackline="track name='Transcripts {}' color=128,0,0".format(file_name.split('/')[-1]))
+            else:
+                raise IOError('\nthe file_name method function needs a name or complete file path with name.\n')
+
     def print_attrs_fields(self):
         for attr_field_print in self.__attr_list.iterkeys():
             print attr_field_print
 
 # checar tamanho total do transcrito
 
-
 def main():
     '''Main Function Description'''
+    print 'Tenho que adicionar attrs no Gene_content...'
+    meu_gtf = GTF_manager(gtf_file='/home/lucas/PycharmProjects/dinar_correlacoes_20016/dados/results/genes-2.gtf')
 
-    meu_gtf = GTF_manager(gtf_file='/work/lncrnas/lucassilva/dinar_correlacoes_20016/data/genes-2.gtf')
+    #print meu_gtf.genes_hash['XLOC_013994'].get_bed6()
     #gtf_object.print_attrs_fields()
-    for gene_id in meu_gtf.gene_list():
-        print gene_id.gene_id, gene_id.locus_coords(), gene_id.locus_length()
-    for transcrito in meu_gtf.transcripts_list():
-        print transcrito.transcript_name, transcrito.transcript_size
-        print transcrito.attrs
+    # meu_gtf.gene_list_to_bed6(save_in_file=True, file_name='teste_genes.bed')
+    # meu_gtf.transcripts_list_to_bed6(save_in_file=True, file_name='teste_transcripts.bed')
+    import random
 
-        # print x.gene_id, x.gene_info,  x.number_of_transcripts
-        # print 'Transcritos:'
-        # x.get_transcripts_ids() #essa porra se nega a ser vista pelo python
+    pc = open('/home/lucas/PycharmProjects/dinar_correlacoes_20016/dados/results/test03.txt').read().replace(',', '.').split('\n')[1:]
+
+    lncrnas = open('/home/lucas/PycharmProjects/dinar_correlacoes_20016/dados/results/test04.txt').read().replace(',', '.').split('\n')[1:]
+
+    pc_hash = {pc_gene.split('\t')[0]: pc_gene.split('\t')[1:] for pc_gene in pc}
+    lncrnas_hash = {ln_gene.split('\t')[0]: ln_gene.split('\t')[1:] for ln_gene in lncrnas}
+
+    pc_list = [pc_gene.split('\t')[0] for pc_gene in pc]
+    lncrnas_list = [ln_gene.split('\t')[0] for ln_gene in lncrnas]
+
+    lncrnas_gtf = copy.deepcopy(meu_gtf)
+    pc_gtf = copy.deepcopy(meu_gtf)
+    lncrnas_gtf.gene_list_select(lncrnas_list).gene_list_to_bed6(save_in_file=True, file_name='lncrnas.bed')
+    bed6_lncrnas = lncrnas_gtf.gene_list_select(lncrnas_list).gene_list_to_bed6()
+
+    bed6_pc = pc_gtf.gene_list_select(pc_list).gene_list_to_bed6()
+
+
+
+    pair_protein = []
+    pair_lncrnas = []
+
+
+
+    from scipy.stats import pearsonr
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+
+    pc_slot = {}
+
+    for vizinhos in bed6_pc.closest(bed6_pc, io =True, d=True):
+        #print 'pc', 'distance:', vizinhos[-1]
+
+        chave_pc = '_'.join(sorted([vizinhos[3], vizinhos[9]]))
+        if chave_pc not in pc_slot:
+            x = map(float, pc_hash[vizinhos[3]])
+            y = map(float, pc_hash[vizinhos[9]])
+            print '_________', chave_pc, '-----------'
+            print vizinhos
+            print x
+            print y
+            print pearsonr(x, y)[0]
+            pair_protein.append(pearsonr(x, y)[0])
+            pc_slot[chave_pc] = ''
+
+
+    for vizinhos in bed6_lncrnas.closest(bed6_pc, io=True, d=True):
+        #print 'lncrnas', 'distance:', vizinhos[-1]
+        x = map(float, lncrnas_hash[vizinhos[3]])
+        y = map(float, pc_hash[vizinhos[9]])
+        pair_lncrnas.append(pearsonr(x, y)[0])
+
+
+
+
+    print len(pair_lncrnas), len(pair_protein)
+
+
+    sns.distplot(np.array(pair_protein), hist=False, color="r",  kde_kws={"shade": False, 'lw': 3, 'alpha': 0.7}, label='protein_protein')
+    sns.distplot(np.array(pair_lncrnas), hist=False, color="b",  kde_kws={"shade": False, 'lw': 3, 'alpha': 0.7}, label='lincRNA_protein')
+
+    #sns.kdeplot(pair_b)
+    plt.show()
+
+
+
+
+
+    #
+    # for gene_id in meu_gtf.gene_list():
+    #     print gene_id.gene_id, gene_id.locus_coords(), gene_id.locus_length(), gene_id.get_bed6()
+    # for transcrito in meu_gtf.transcripts_list():
+    #     print transcrito.transcript_name, transcrito.transcript_length
+    #     print transcrito.attrs
+    #     print transcrito.get_bed6()
 
 if __name__ == '__main__':
     sys.exit(main())
