8cfed9a159290284f79828cc7895a8dcd2dfab13
lucas <bioinfolucas@gmail.com>
2016-01-13 11:21:10 -0200

    new functions added

--- scripts/GTF_manager.py
+++ scripts/GTF_manager.py
@@ -29,6 +29,7 @@ class Transcript():
 
         fields_to_parse = {field.strip(' ').split(' ')[0]:field.strip(' ').split(' ')[1]
                            for field in self.exons[-1][-1].strip(';').split(';')} # get the last exon to parse attrs
+        #print 'ftp', len(fields_to_parse), 'attr_list', len([x for x in self.__attr_list])
         hash_to_load_return = {}
         for attr in self.__attr_list:
             if attr in fields_to_parse:
@@ -52,25 +53,45 @@ class Gene_content():
         self.strand = strand  # checar se strand existe na posicao correta else: return error
         self.transcripts_ids = transcripts_id_hash #sorted_dict
         self.__child_transcript_possible_fields = attr_transcript_field #hash com os valores que os transcritos vao possuir
-        self.transcripts_list = self.__parse_transcripts()
-        self.number_of_transcripts = len(self.transcripts_list)
+        self.__transcripts_list = self.__parse_transcripts()
+        self.number_of_transcripts = len(self.__transcripts_list)
 
+    def locus_coords(self):
+        #print type(self.__transcripts_list), self.__transcripts_list
+        chr = self.__transcripts_list[0].exons[0][0]
+        strand = self.__transcripts_list[0].exons[0][6]
+        if strand == "-":
+
+            return (chr,
+            min([int(end[3]) for end_parse in self.__transcripts_list for end in end_parse.exons]),
+            max([int(start[4]) for start_parse in self.__transcripts_list for start in start_parse.exons]))
+        elif strand == "+":
+            return (chr,
+            min([int(start[3]) for start_parse in self.__transcripts_list  for start in start_parse.exons]),
+            max([int(end[4]) for end_parse in self.__transcripts_list for end in end_parse.exons]))
+
+    def locus_length(self):
+        chr, start_l, end_l = self.locus_coords()
+        return end_l - start_l
+
+    def transcripts_list(self):
+        return self.__parse_transcripts()
 
     def get_transcripts_ids(self):
         out_ids_trans = []
-        #return list(set(
+
         for trans_line in self.transcripts_ids.iterkeys():
-            print trans_line
-            #out_ids_trans.append(re.search('transcript_id "(\S*)"', trans_line[-1]).group(1))
-        # return list(set(out_ids_trans))
+            #print trans_line
+            out_ids_trans.append(re.search('transcript_id "(\S*)"', trans_line[-1]).group(1))
+        return out_ids_trans
 
 
 
     def __parse_transcripts(self):
         hash_transcripts_to_parse = {}
-        return {Transcript(transcript_name=ts_key,
+        return [Transcript(transcript_name=ts_key,
                            exons_array=ts_values_array,
-                           attr_list=self.__child_transcript_possible_fields) for ts_key, ts_values_array in self.transcripts_ids.iteritems()}
+                           attr_list=self.__child_transcript_possible_fields) for ts_key, ts_values_array in self.transcripts_ids.iteritems()]
 
 
 class GTF_manager():
@@ -101,9 +122,10 @@ class GTF_manager():
                     raise IOError("\n The Line: \n{line}\n don't have a regular gene_id field!".format(line=line))
 
         gene_list_to_return = {}
-        print '\n \n Converting gene to proper format\n'
-        for key_gene, gene_value in hash_gtf_file.iteritems():
-
+        print 'Converting gene to proper format...'
+        for gene_unpack in tqdm(hash_gtf_file):
+            key_gene = gene_unpack
+            gene_value = hash_gtf_file[key_gene]
             gene_id = key_gene
             gene_info = gene_value[0][1]
             strand = gene_value[0][6]
@@ -121,7 +143,7 @@ class GTF_manager():
                          strand=strand,
                          transcripts_id_hash=transcript_hash,
                          info_gene_str=gene_info,
-                         attr_transcript_field=self.__attr_list.iterkeys())
+                         attr_transcript_field=self.__attr_list)
 
             if gene_id in gene_list_to_return:
                 raise IOError('\n Duplicated gene_id\n {gene_id}\n'.format(gene_id=gene_id))
@@ -138,7 +160,12 @@ class GTF_manager():
     def gene_list(self):
         return [gene_content  for gene_content in self.genes_hash.itervalues()]
 
-
+    def transcripts_list(self):
+        transcript_list_return = []
+        for gene_in_list in self.gene_list():
+            for transcript_in_list in gene_in_list.transcripts_list():
+                transcript_list_return.append(transcript_in_list)
+        return transcript_list_return
 
     def print_attrs_fields(self):
         for attr_field_print in self.__attr_list.iterkeys():
@@ -150,11 +177,13 @@ class GTF_manager():
 def main():
     '''Main Function Description'''
 
-    meu_gtf = GTF_manager(gtf_file='/home/lucas/PycharmProjects/dinar_correlacoes_20016/dados/results/genes-2.gtf')
+    meu_gtf = GTF_manager(gtf_file='/work/lncrnas/lucassilva/dinar_correlacoes_20016/data/genes-2.gtf')
     #gtf_object.print_attrs_fields()
     for gene_id in meu_gtf.gene_list():
-        for transcrito in gene_id.transcripts_list:
-            print transcrito.transcript_name, transcrito.transcript_size, transcrito.attrs['gene_type']
+        print gene_id.gene_id, gene_id.locus_coords(), gene_id.locus_length()
+    for transcrito in meu_gtf.transcripts_list():
+        print transcrito.transcript_name, transcrito.transcript_size
+        print transcrito.attrs
 
         # print x.gene_id, x.gene_info,  x.number_of_transcripts
         # print 'Transcritos:'
